a206602b40244ac15aac6fe09079fbdff0dc9c32
6323612 <6323612@host.docker.internal>
2025-10-15 15:07:27 -0400

    setup coordinate values

--- src/lab06_daniel/HouseWithWindowPanesLab.java
+++ src/lab06_daniel/HouseWithWindowPanesLab.java
@@ -5,6 +5,7 @@
 package lab06_daniel;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -16,8 +17,26 @@ public class HouseWithWindowPanesLab extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    
     @Override
     public void start(Stage stage) {
+        /*
+        DRAW A HOUSE
+        - Base, Roof, Door
+        */
+        final int SCENE_WIDTH = 520, SCENE_HEIGHT = 520;
+        final int HOUSE_POS_X = 110, HOUSE_POS_Y = 400;
+        final int HOUSE_SIZE_X = 300, HOUSE_SIZE_Y = 200;
+        final int ROOF_HEIGHT = 100;
+        final int BASE_HEIGHT = 50;
+        
+        
+        Pane root = new Pane();
+        
+        Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+        stage.setScene(scene);
+        stage.setTitle("House or something");
+        stage.show();
     }
     
 }
