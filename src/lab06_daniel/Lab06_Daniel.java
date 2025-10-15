069a63084ffed6e64732847a2e334dd7dd746df6
Daniel Orejuela Liu <6323612@edu.vaniercollege.qc.ca>
2025-10-15 14:36:05 -0400

    first comm

---
+++ src/lab06_daniel/Lab06_Daniel.java
@@ -0,0 +1,28 @@
+package lab06_daniel;
+import javafx.application.Application; 
+import javafx.stage.Stage; 
+import javafx.scene.Scene; 
+import javafx.scene.layout.Pane; 
+import javafx.scene.paint.Color; 
+import javafx.scene.shape.Rectangle; 
+import javafx.scene.shape.Line; 
+import javafx.scene.shape.Circle;
+
+/**
+ *
+ * @author 6323612
+ */
+public class Lab06_Daniel extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    @Override
+    public void start(Stage stage) {
+    }
+    
+}
