98429ac95ec25b5d642b8d78260eae97ebb303cd
6323612 <6323612@host.docker.internal>
2025-10-15 15:01:10 -0400

    task2 first commit

---
+++ src/lab06_daniel/HouseWithWindowPanesLab.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab06_daniel;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 6323612
+ */
+public class HouseWithWindowPanesLab extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) {
+    }
+    
+}
